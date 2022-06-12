283692fc7728ef34f9f9673b6eb8dd4eec0d0e79
LiliyaEremenko <97868170+LiliyaEremenko@users.noreply.github.com>
2022-06-12 17:52:34 +0700

    73

--- index.php
+++ index.php
@@ -2,17 +2,12 @@
 require_once 'tag.php';
 require_once 'form.php';
 require_once 'input.php';
+require_once 'submit.php';
 
-$form = (new Form)->setAttrs(['action' => '', 
-		'method' => 'GET']);
-	
- 	echo $form->open();
-		echo (new Input)->setAttr('name', 'a');
-		echo (new Input)->setAttr('name', 'b');
-		echo (new Input)->setAttr('name', 'c');
-		echo (new Input)->setAttr('name', 'd');
-		echo (new Input)->setAttr('name', 'e');
-		echo (new Input)->setAttr('type', 'submit');
-	echo $form->close();
-echo $sum;
+$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
+
+echo $form->open();
+echo (new Input)->setAttr('name', 'first');
+echo new Submit;
+echo $form->close();
 ?>
