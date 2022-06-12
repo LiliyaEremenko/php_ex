7d9983f0ce1fd69c9ff81c1e11fc330f077d9e35
LiliyaEremenko <97868170+LiliyaEremenko@users.noreply.github.com>
2022-06-12 17:56:59 +0700

    74

--- index.php
+++ index.php
@@ -3,11 +3,13 @@ require_once 'tag.php';
 require_once 'form.php';
 require_once 'input.php';
 require_once 'submit.php';
+require_once 'password.php';
 
 $form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
 echo $form->open();
-echo (new Input)->setAttr('name', 'first');
+echo (new Input)->setAttr('name', 'login');
+echo (new Password)->setAttr('name', 'pass');
 echo new Submit;
 echo $form->close();
 ?>
