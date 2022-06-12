72b134f4e4d8924299fabb060bccbb1fe0ac892f
LiliyaEremenko <97868170+LiliyaEremenko@users.noreply.github.com>
2022-06-12 18:03:39 +0700

    75

---
+++ hidden.php
@@ -0,0 +1,8 @@
+<?php
+class Hidden extends Input {
+    public function __construct() {
+        $this->setAttr('type', 'hidden');
+        parent::__construct();
+    }
+}
+?>
--- index.php
+++ index.php
@@ -4,12 +4,13 @@ require_once 'form.php';
 require_once 'input.php';
 require_once 'submit.php';
 require_once 'password.php';
+require_once 'hidden.php';
 
 $form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
 echo $form->open();
 echo (new Input)->setAttr('name', 'login');
-echo (new Password)->setAttr('name', 'pass');
+echo (new Hidden)->setAttr('name', 'pass')->setAttr('value', 'empty');
 echo new Submit;
 echo $form->close();
 ?>
