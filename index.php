63398f0e01ad4ef0cfb28b7483a3f2d313dd97ab
LiliyaEremenko <97868170+LiliyaEremenko@users.noreply.github.com>
2022-06-12 14:00:56 +0700

    71

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 require_once 'tag.php';
 require_once 'image.php';
+require_once 'link.php';
+require_once 'htmlList.php';
 
-echo (new Image())->setAttrs(['src' => 'mafuyu.png', 'width' => '300px', 'height' => '200px']);
+$list = new HtmlList('ul');
+	
+	echo $list
+		->addItem((new ListItem())->setText('item1'))
+		->addItem((new ListItem())->setText('item2'))
+		->addItem((new ListItem())->setText('item3'));
+
+$ul = new Ul;
+$ol = new Ol;
+
+echo $ul
+	->addItem((new ListItem())->setText('one'))
+	->addItem((new ListItem())->setText('two'));
+
+echo $ol
+	->addItem((new ListItem())->setText('one'))
+	->addItem((new ListItem())->setText('two'));
 ?>
