5a180d7afa9c7adb0d887dbad83019a6b51d9dfb
LiliyaEremenko <97868170+LiliyaEremenko@users.noreply.github.com>
2022-06-13 13:26:48 +0700

    77

---
+++ checkbox.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'hidden.php';
+
+class Checkbox extends Tag {
+    public function __construct() {
+        $this->setAttrs(['type' => 'checkbox', 'value' => '1']);
+        parent::__construct('input');
+    }
+
+    public function open() {
+        $name = $this->getAttr('name');
+        if ($name) {
+            $hidden = (new Hidden)->setAttrs(['name' => $name, 'value' => '0']);
+            if (isset($_REQUEST[$name])) {
+                $value = $_REQUEST[$name];
+                if ($value == 1) {
+                    $this->setAttr('checked');
+                }
+                else {
+                    $this->removeAttr('checked');
+                }
+            }
+            return $hidden->open() . parent::open();
+        }
+        else {
+            return parent::open();
+        }
+    }
+
+    public function __toString() {
+        return $this->open();
+    }
+}
+?>
--- index.php
+++ index.php
@@ -3,14 +3,16 @@ require_once 'tag.php';
 require_once 'form.php';
 require_once 'input.php';
 require_once 'submit.php';
-require_once 'password.php';
 require_once 'hidden.php';
-require_once 'textarea.php';
+require_once 'checkbox.php';
+require_once 'radio.php';
 
-$form = (new Form)->setAttrs(['method' => 'GET']);
+$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
 echo $form->open();
-echo (new Textarea)->setAttr('name', 'message');
+echo (new Radio)->setAttr('name', 'check');
+echo (new Radio)->setAttr('name', 'check');
+echo (new Input)->setAttr('name', 'login');
 echo new Submit;
 echo $form->close();
 ?>
